a6479c845d81f29391fbb8272c790a28df0e5956
Matias Wysoczanski <matiaswyso@gmail.com>
2024-09-06 14:44:25 -0300

    form actualizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
-
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/HeroSection";
 import About from "./components/About/About";
-import SubscriptionForm from './components/Form/Form';
+import SubscriptionForm from "./components/Form/Form";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
     <main className="landing">
-       <Navbar />
-       <Hero />
-       <About />
-       <SubscriptionForm />
-       
-       
-
+      <Navbar />
+      <Hero />
+      <About />
+      <SubscriptionForm />
+      <Footer />
     </main>
   );
 }
