f57cb22f3fd00ebab8632c19d94351b36f744b37
julianvidela <Julianvyd@gmail.com>
2024-09-06 14:52:00 -0300

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,17 @@
-
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/HeroSection";
 import About from "./components/About/About";
-<<<<<<< HEAD
 import ArticleSection from "./components/ArcticleSection/ArticleSection";
-
-=======
-import SubscriptionForm from './components/Form/Form';
->>>>>>> 825bde46a97fcb8120066eed62a5593e9ab19473
+import SubscriptionForm from "./components/Form/Form";
 
 export default function Home() {
   return (
     <main className="landing">
-       <Navbar />
-       <Hero />
-       <ArticleSection />
-       <About />
-       <SubscriptionForm />
-       
-       
-
+      <Navbar />
+      <Hero />
+      <ArticleSection />
+      <About />
+      <SubscriptionForm />
     </main>
   );
 }
