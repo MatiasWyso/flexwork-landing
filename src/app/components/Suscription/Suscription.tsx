730a9054168316c689c614fbd9ee631b079f5ae3
Matias Wysoczanski <matiaswyso@gmail.com>
2024-09-17 17:16:15 -0300

    z-index del form

--- src/app/components/Suscription/Suscription.tsx
+++ src/app/components/Suscription/Suscription.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import Form from "./Form/Form";
 import { MultiStepLoaderDemo } from "./Loader/test";
 import "./Suscription.scss";
+import { div } from "framer-motion/client";
 
 const Suscription = () => {
   return (
-    <section id="suscripcion">
-      <div className="Multistep">
-        <MultiStepLoaderDemo />
-      </div>
-      <div className="Form">
-        <Form />
+    <section className="suscription-container">
+      <div id="suscripcion">
+        <div className="grilla"></div>
+        <div className="otromas">
+          <div className="Multistep">
+            <MultiStepLoaderDemo />
+          </div>
+          <div className="Form">
+            <Form />
+          </div>
+        </div>
       </div>
     </section>
   );
