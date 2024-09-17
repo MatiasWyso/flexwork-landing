e1aedc46aeef930f354755d3ec4f6dc755d575ee
julianvidela <Julianvyd@gmail.com>
2024-09-17 16:35:14 -0300

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,23 @@
-
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/HeroSection";
 import ArticleSection from "./components/ArcticleSection/ArticleSection";
 import Info from "./components/Info/Info";
-import Footer from "./components/Footer/Footer"; 
+import Footer from "./components/Footer/Footer";
 import "./page.scss";
 import Suscription from "./components/Suscription/Suscription";
 
-
 export default function Home() {
-
- 
   return (
     <main className="landing">
-      
-
       <Navbar />
       <Hero />
 
-      <hr className="hr__landing"/> 
-
-      
+      <hr className="hr__landing" />
 
       <ArticleSection />
       <Suscription />
       <Info />
-      <Footer /> 
+      <Footer />
     </main>
   );
 }
